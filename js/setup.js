c6341cd9f17fa82b56b149392a442907c291f563
validcoderu <validcoderu@gmail.com>
2020-02-10 14:05:11 +0100

    fuction optimization

--- js/setup.js
+++ js/setup.js
@@ -7,42 +7,37 @@ var SUR_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вал�
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb (241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var renderWizardName = function (names, surnames) {
+var getWizardFullName = function (names, surnames) {
   var wizardName = Math.floor(Math.random() * names.length);
   var wizardSurname = Math.floor(Math.random() * surnames.length);
   return names[wizardName] + ' ' + surnames[wizardSurname];
 };
 
-var getRandomCoatColor = function (coatColors) {
-  var wizardCoatColor = Math.floor(Math.random() * coatColors.length);
-  return coatColors[wizardCoatColor];
-};
-
-var getRandomEyesColor = function (eyesColors) {
-  var wizardEyeColor = Math.floor(Math.random() * eyesColors.length);
-  return eyesColors[wizardEyeColor];
+var getRandomColor = function (colors) {
+  var randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
 };
 
 var wizards = [
   {
-    name: renderWizardName(FIRST_NAMES, SUR_NAMES),
-    coatColor: getRandomCoatColor(COAT_COLORS),
-    eyesColor: getRandomEyesColor(EYES_COLORS)
+    name: getWizardFullName(FIRST_NAMES, SUR_NAMES),
+    coatColor: getRandomColor(COAT_COLORS),
+    eyesColor: getRandomColor(EYES_COLORS)
   },
   {
-    name: renderWizardName(FIRST_NAMES, SUR_NAMES),
-    coatColor: getRandomCoatColor(COAT_COLORS),
-    eyesColor: getRandomEyesColor(EYES_COLORS)
+    name: getWizardFullName(FIRST_NAMES, SUR_NAMES),
+    coatColor: getRandomColor(COAT_COLORS),
+    eyesColor: getRandomColor(EYES_COLORS)
   },
   {
-    name: renderWizardName(FIRST_NAMES, SUR_NAMES),
-    coatColor: getRandomCoatColor(COAT_COLORS),
-    eyesColor: getRandomEyesColor(EYES_COLORS)
+    name: getWizardFullName(FIRST_NAMES, SUR_NAMES),
+    coatColor: getRandomColor(COAT_COLORS),
+    eyesColor: getRandomColor(EYES_COLORS)
   },
   {
-    name: renderWizardName(FIRST_NAMES, SUR_NAMES),
-    coatColor: getRandomCoatColor(COAT_COLORS),
-    eyesColor: getRandomEyesColor(EYES_COLORS)
+    name: getWizardFullName(FIRST_NAMES, SUR_NAMES),
+    coatColor: getRandomColor(COAT_COLORS),
+    eyesColor: getRandomColor(EYES_COLORS)
   }
 ];
 
